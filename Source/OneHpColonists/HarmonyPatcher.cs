c848ab335715b14a26b633be91b1148430748bb6
DESKTOP-T2HO7ER\admin <tarasko4444@gmail.com>
2024-05-18 00:32:36 +0300

    v1.0

---
+++ Source/OneHpColonists/HarmonyPatcher.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace OneHpColonists;
+
+[StaticConstructorOnStartup]
+public class HarmonyPatcher
+{
+    static HarmonyPatcher()
+    {
+        var harmony = new Harmony("rimworld.mod.me.onehpcolonists");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(DamageWorker_AddInjury), "ApplyToPawn")]
+public static class Patch
+{
+    public static void Postfix(DamageInfo dinfo, Pawn pawn, ref DamageWorker.DamageResult __result)
+    {
+        if (pawn.IsColonist && !__result.deflected)
+        {
+            pawn.Kill(dinfo);
+        }
+    }
+}
